b3cd3359a541ae5737007a8f91f1e9adaf283df5
alexxw-lz <113810007+alexxw-lz@users.noreply.github.com>
2022-10-02 16:29:00 +0800

    update funcs

--- get_chat_id.py
+++ get_chat_id.py
@@ -4,7 +4,7 @@ import cfg
 
 
 app = Client(
-	session_name = 'user',
+	name = 'user',
 	api_id = cfg.API_ID,
 	api_hash = cfg.API_HASH,
 	phone_number = cfg.PHONE_NUMBER,
@@ -13,7 +13,7 @@ app = Client(
 )
 async def main():
 	await app.start()
-	async for dialog in app.iter_dialogs():
+	async for dialog in app.get_dialogs():
 		print((dialog.chat.first_name+' '+dialog.chat.last_name if dialog.chat.last_name else dialog.chat.first_name)or dialog.chat.title, dialog.chat.id)
 
 app.run(main())
